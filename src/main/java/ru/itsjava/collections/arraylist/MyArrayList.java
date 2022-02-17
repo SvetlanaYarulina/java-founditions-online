24413f6e72afcdf3389fbbf7d9409c296e6eee31
SvetlanaYarulina <svetik_ru86@mail.ru>
2022-02-17 13:25:37 +0500

    MyArrayListPractice4.java

--- src/main/java/ru/itsjava/collections/arraylist/MyArrayList.java
+++ src/main/java/ru/itsjava/collections/arraylist/MyArrayList.java
@@ -5,6 +5,7 @@ public class MyArrayList {
     private int realSize;
     private Object[] array;
     private java.lang.Object Object;
+    private java.lang.Object element;
 
     public MyArrayList() {
         array = new Object[DEFAULT_CAPACITY];
@@ -17,16 +18,14 @@ public class MyArrayList {
 
     //проверяем является ли объект пустым или нет
     public boolean isEmpty() {
-        for (int i = 0; i < array.length; i++) {
-            return (array[i] == null);
-        }
+        realSize = 0;
         return false;
     }
 
     // проверяем наличие объекта в списке
     public boolean contains(Object o) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == o) {
+        for (int i = 0; i < realSize; i++) {
+            if (array[i] == o && array[i].equals(o)) {
                 return true;
             }
         }
@@ -72,18 +71,46 @@ public class MyArrayList {
         realSize = 0;
     }
 
-
+    // получить объект с указанным индексом
+    //Если index < 0 или index >= максимального количества элементов списка,
+    // будет выброшено исключение IndexOutOfBoundsException.
     public Object get(int index) {
-        return null;
+        checkIndex(index);
+            return array[index];
     }
 
+
+    // Замена элемента в указанной позиции index на переданный element. Индекс также должен быть больше нуля и
+    // меньше индекса последнего элемента, иначе будет выброшено исключение IndexOutOfBoundsException
+    //  размер списка не меняется.
     public Object set(int index, Object element) {
-        return null;
+        checkIndex(index);
+        for (int i = 0; i < array.length; i++) {
+            if (i == index) {
+                array[i] = element;
+            }
+        }
+            array[index] = element;
+            return element;
     }
-
+    // Этот метод вставляет указанный элемент в указанную позицию в этом списке. Он сдвигает элемент,
+    // находящийся в данный момент в этом положении (если таковой имеется),
+    // и любые последующие элементы вправо (добавит один к их индексам).
     public void add(int index, Object element) {
-
+        if (realSize == array.length) {
+            Object[] resArray = new Object[array.length * 3 / 2 + 1];
+            System.arraycopy(array, 0, resArray, 0, array.length);
+            array = resArray;
+        }
+        realSize++;
+        int insertIndex = index;
+        for (int i = array.length - 2; i >= insertIndex; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = element;
     }
+
+
 
     public Object remove(int index) {
         checkIndex(index);
@@ -96,6 +123,7 @@ public class MyArrayList {
         return resElement;
     }
 
+    //  проверяем корректен ли индекс
     private void checkIndex(int index) {
         if (!isCorrectIndex(index)) {
             throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
@@ -103,18 +131,40 @@ public class MyArrayList {
     }
 
     private boolean isCorrectIndex(int index) {
-        if ((index > -1) || (index < realSize)) {
+        if ((index > -1) && (index < realSize)) {
             return true;
         }
         return false;
     }
 
+        // Этот метод возвращает индекс первого вхождения указанного элемента
+        // в данный список или -1, если список не содержит элемента .
     public int indexOf(Object o) {
-        return 0;
-    }
+            if (o == null) {
+                for (int i = 0; i < realSize; i++)
+                    if (array[i]==null)
+                        return i;
+            } else {
+                for (int i = 0; i < realSize; i++)
+                    if (o.equals(array[i]))
+                        return i;
+            }
+            return -1;
+        }
+
+       // находит индекс последнего вхождения в списке данного элемента:
 
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = realSize-1; i >= 0; i--)
+                if (array[i]==null)
+                    return i;
+        } else {
+            for (int i = realSize-1; i >= 0; i--)
+                if (o.equals(array[i]))
+                    return i;
+        }
+        return -1;
     }
 
     @Override
--- src/main/java/ru/itsjava/collections/arraylist/MyArrayListPractice.java
+++ src/main/java/ru/itsjava/collections/arraylist/MyArrayListPractice.java
@@ -52,6 +52,9 @@ public class MyArrayListPractice {
         MyArrayList list1 = new MyArrayList();
         list1.isEmpty();
         System.out.println("Пустой ли список:" + " " + list1.isEmpty());
+        int index5 = list1.indexOf("element1");
+        System.out.println("index5 = " + index5);
+
 
         list1.add("element1");
         list1.add("element2");
@@ -65,7 +68,43 @@ public class MyArrayListPractice {
         list1.clear();
         System.out.println(list1);
 
+        MyArrayList list2 = new MyArrayList();
+        list2.add("element1");
+        list2.add("element2");
+        list2.add("element3");
+        list2.add("element3");
 
+        System.out.println(list2);
+        int index1 = list2.indexOf("element1");
+        System.out.println("index1 = " + index1);
+        int index2 = list2.indexOf("element4");
+        System.out.println("index2 = " + index2);
+
+        int lastIndex = list2.lastIndexOf("element3");
+        System.out.println("lastIndex:" + " " + lastIndex);
+        int lastIndex1 = list2.lastIndexOf("element5");
+        System.out.println("lastIndex1:" + " " + lastIndex1);
+
+        System.out.println("Первый элемент в списке:" +" " + list2.get(0));
+        System.out.println("Третийэлемент в списке:" +" " + list2.get(2));
+       // System.out.println("Десятый элемент в списке:" +" " + list2.get(9));
+        list2.set(0, 11);
+        System.out.println(list2);
+        list2.set(3, 15);
+        System.out.println(list2);
+
+        list2.add(0, "element5");
+        System.out.println(list2);
+
+        list2.add(3, "18");
+        System.out.println(list2);
+
+
+
+
+        MyArrayList list3 = new MyArrayList();
+        int index0 = list3.indexOf(null);
+        System.out.println("index0 = " + index0);
     }
 }
 
