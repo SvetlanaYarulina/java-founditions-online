1ce05c595e16c071538cf632d9742e28b3d65e07
SvetlanaYarulina <svetik_ru86@mail.ru>
2022-02-24 18:29:43 +0500

    MyLinkedListPractice.java

--- src/main/java/ru/itsjava/collections/linkedlist/MyLinkedList.java
+++ src/main/java/ru/itsjava/collections/linkedlist/MyLinkedList.java
@@ -1,14 +1,14 @@
 package ru.itsjava.collections.linkedlist;
 
 public class MyLinkedList {
-    private Node head; // головной узел
+    private Node head; // первый элемент (узел)
 
     public int size() {
         int realSize;
         if (head == null) {
             realSize = 0;
         } else {
-            Node curNode = head;
+            Node curNode = head;           //  curNode - текущий узел
             realSize = 1;  // увеличиваем на 1
             while (curNode.getNext() != null) {
                 curNode = curNode.getNext();
@@ -20,9 +20,6 @@ public class MyLinkedList {
 
 
     public boolean isEmpty() {
-        if (head != null) {
-            return false;
-        }
              return head == null;
             }
 
@@ -57,30 +54,138 @@ public class MyLinkedList {
             return true;
         }
 
-
+    // СТРОКА1 -> null
+    // СТРОКА1 -> СТРОКА2 -> СТРОКА3 -> null
         public boolean remove (Object o){
-            return false;
+            if (head == null)
+                return false;
+
+            if (head.getValue().equals(o)) {
+                head = head.getNext();
+                return true;
+            }
+
+            if (head.getNext() == null) return false;
+
+            Node curNode = head;
+            Node prevNode = head;     //  prevNode предыдущий узел
+
+            while ((curNode = curNode.getNext()) != null) {
+                if (curNode.getValue().equals(o)) {
+                    break;
+                }
+                prevNode = prevNode.getNext();
+            }
+
+            if (curNode == null) return false;
+
+            prevNode.setNext(curNode.getNext());
+            curNode.setNext(null);
+            return true;
         }
+
 
         public void clear () {
+            Node prevNode = head; //присваеваем переменной предыдущего значения первый элемент
+            Node curNode = head; //присваеваем переменной текущего значения первый элемент
 
+
+            while ((curNode = curNode.getNext()) != null) { //пробегаемся по листу
+                prevNode.setValue(null);
+                prevNode.setNext(null);
+                prevNode = curNode; //присваиваем предыдущему значению стоящий после него элемент
+            }
+            head = null;
         }
+
 
         public Object get ( int index){
-            return null;
+            checkIndex(index);
+            int count = 0;
+            Node curNode = head;
+            while (curNode.getNext() != null) {
+                if (count++ == index) {
+                    break;
+                }
+                curNode = curNode.getNext();
+            }
+
+
+            return curNode.getValue();
         }
 
-        public Object set ( int index, Object element){
-            return null;
+
+    public Object set ( int index, Object element){
+            checkIndex(index);
+            Object resValue = null;
+            int count = 0;
+            Node curNode = head;
+            while (curNode.getNext() != null) {
+                if (count++ == index) {
+                    resValue = curNode.getValue();
+                    curNode.setValue(element);
+                }
+                curNode = curNode.getNext();
+            }
+            return resValue;
         }
 
-        public void add ( int index, Object element){
+
+
+
+    public void add ( int index, Object element){
 
         }
 
-        public Object remove ( int index){
-            return null;
+        public Object remove ( int index) {
+            checkIndex(index); // проверяем корректность индекса
+            if (index == 0) { // проеряем если индекс равен 0, то удаляем head
+                Object resValue = head.getValue(); //присваиваем удаляемому значению значение с нулевым индексом (первый элемент)
+                if (head.getNext() == null) { //проверяем, единственный ли это элемент в листе
+                    head = null;  //если единственный, зануляем его
+                } else { // иначе
+                    head= head.getNext();// присваиваем  head следущий элемент
+                }
+                return resValue; //возвращаем удаляемый элемент
+            }
+            // нужно пробежаться до того элемента, который мы хотим удалить
+            Node curNode = head;
+            Node prevNode = head; // node до curNode
+            int count = 0; // создаём счетчик
+            while ((curNode = curNode.getNext()) != null) { // бежим до конца масссива
+                count++; //увеличиваем счётчик на 1
+                if (count == index) { //если счётчик равен искомому индексу, значит нашли нужный элемент
+                    break; //выходим из цикла
+                }
+                prevNode = prevNode.getNext(); //присваиваем предыдущему значению стоящий после него элемент
+            }
+            //если удаляем с конца
+            if (curNode == null) // проверяем если curNode не null
+                return null;
+            Object resValue = curNode.getValue(); //присваиваем переменной значение текущего элемента
+
+            if (curNode.getNext() == null) { //проверяем, если текущий элемент последний
+                prevNode.setNext(null); //зануляем элемент после предыдущего элемента
+            } else {
+                prevNode.setNext(curNode.getNext()); //присваиваем элементу после предыдущего, значение следующего за текущим
+                curNode.setNext(null); // зануляем следующий элемент после текущего
+            }
+
+            return resValue;  //возвращаем значение удаляемого элемента
         }
+
+    private void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
+    }
+
+    private boolean isCorrectIndex(int index) {
+        if ((index > -1) && (index < size())) {  // вызываем метод size
+            return true;
+        }
+        return false;
+    }
 
         public int indexOf (Object o){
             return 0;
--- src/main/java/ru/itsjava/collections/linkedlist/MyLinkedListPractice.java
+++ src/main/java/ru/itsjava/collections/linkedlist/MyLinkedListPractice.java
@@ -25,6 +25,18 @@ public class MyLinkedListPractice {
         list.isEmpty();
         System.out.println("Пустой ли список:" + " " + list.isEmpty());
 
+        list.remove(0);
+        System.out.println(list);
+
+        System.out.println("list.get(0) = " + list.get(0));
+        System.out.println("list.get(1) = " + list.get(1));
+      //  System.out.println("list.get(2) = " + list.get(2));
+
+        list.set(0, "Строка4");
+        System.out.println(list);
+
+        list.clear();
+        System.out.println(list);
 
 
     }
