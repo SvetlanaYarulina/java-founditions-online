aacd02aa8f9f9ae74e256cff39ba6097f0e84563
SvetlanaYarulina <svetik_ru86@mail.ru>
2022-02-25 12:27:56 +0500

    MyLinkedListPractice.java

--- src/main/java/ru/itsjava/collections/linkedlist/MyLinkedList.java
+++ src/main/java/ru/itsjava/collections/linkedlist/MyLinkedList.java
@@ -56,97 +56,96 @@ public class MyLinkedList {
     // СТРОКА1 -> null
     // СТРОКА1 -> СТРОКА2 -> СТРОКА3 -> null
     public boolean remove(Object o) {
+        return false;
+    }
+
+    public void clear() {
+        Node prevNode = head; //присваеваем переменной предыдущего значения первый элемент
+        Node curNode = head; //присваеваем переменной текущего значения первый элемент
+
+
+        while ((curNode = curNode.getNext()) != null) { //пробегаемся по листу
+            prevNode.setValue(null);
+            prevNode.setNext(null);
+            prevNode = curNode; //присваиваем предыдущему значению стоящий после него элемент
+        }
+        head = null;
+    }
+
+
+    public Object get(int index) {
+        checkIndex(index);
+        int count = 0;
+        Node curNode = head;
+        while (curNode.getNext() != null) {
+            if (count++ == index) {
+                break;
+            }
+            curNode = curNode.getNext();
+        }
+
+
+        return curNode.getValue();
+    }
+
+
+    public Object set(int index, Object element) {
+        checkIndex(index);
+        Object resValue = null;
+        int count = 0;
+        Node curNode = head;
+        while (curNode.getNext() != null) {
+            if (count++ == index) {
+                resValue = curNode.getValue();
+                curNode.setValue(element);
+                break;
+            }
+            curNode = curNode.getNext();
+        }
+        return resValue;
+    }
+
+
+    public void add(int index, Object element) {
 
     }
 
-        public void clear () {
-            Node prevNode = head; //присваеваем переменной предыдущего значения первый элемент
-            Node curNode = head; //присваеваем переменной текущего значения первый элемент
-
-
-            while ((curNode = curNode.getNext()) != null) { //пробегаемся по листу
-                prevNode.setValue(null);
-                prevNode.setNext(null);
-                prevNode = curNode; //присваиваем предыдущему значению стоящий после него элемент
+    public Object remove(int index) {
+        checkIndex(index); // проверяем корректность индекса
+        if (index == 0) { // проеряем если индекс равен 0, то удаляем head
+            Object resValue = head.getValue(); //присваиваем удаляемому значению значение с нулевым индексом (первый элемент)
+            if (head.getNext() == null) { //проверяем, единственный ли это элемент в листе
+                head = null;  //если единственный, зануляем его
+            } else { // иначе
+                head = head.getNext();// присваиваем  head следущий элемент
             }
-            head = null;
+            return resValue; //возвращаем удаляемый элемент
+        }
+        // нужно пробежаться до того элемента, который мы хотим удалить
+        Node curNode = head;
+        Node prevNode = head; // node до curNode
+        int count = 0; // создаём счетчик
+        while ((curNode = curNode.getNext()) != null) { // бежим до конца масссива
+            count++; //увеличиваем счётчик на 1
+            if (count == index) { //если счётчик равен искомому индексу, значит нашли нужный элемент
+                break; //выходим из цикла
+            }
+            prevNode = prevNode.getNext(); //присваиваем предыдущему значению стоящий после него элемент
+        }
+        //если удаляем с конца
+        if (curNode == null) // проверяем если curNode не null
+            return null;
+        Object resValue = curNode.getValue(); //присваиваем переменной значение текущего элемента
+
+        if (curNode.getNext() == null) { //проверяем, если текущий элемент последний
+            prevNode.setNext(null); //зануляем элемент после предыдущего элемента
+        } else {
+            prevNode.setNext(curNode.getNext()); //присваиваем элементу после предыдущего, значение следующего за текущим
+            curNode.setNext(null); // зануляем следующий элемент после текущего
         }
 
-
-        public Object get ( int index){
-            checkIndex(index);
-            int count = 0;
-            Node curNode = head;
-            while (curNode.getNext() != null) {
-                if (count++ == index) {
-                    break;
-                }
-                curNode = curNode.getNext();
-            }
-
-
-            return curNode.getValue();
-        }
-
-
-    public Object set ( int index, Object element){
-            checkIndex(index);
-            Object resValue = null;
-            int count = 0;
-            Node curNode = head;
-            while (curNode.getNext() != null) {
-                if (count++ == index) {
-                    resValue = curNode.getValue();
-                    curNode.setValue(element);
-                }
-                curNode = curNode.getNext();
-            }
-            return resValue;
-        }
-
-
-
-
-    public void add ( int index, Object element){
-
-        }
-
-        public Object remove ( int index) {
-            checkIndex(index); // проверяем корректность индекса
-            if (index == 0) { // проеряем если индекс равен 0, то удаляем head
-                Object resValue = head.getValue(); //присваиваем удаляемому значению значение с нулевым индексом (первый элемент)
-                if (head.getNext() == null) { //проверяем, единственный ли это элемент в листе
-                    head = null;  //если единственный, зануляем его
-                } else { // иначе
-                    head= head.getNext();// присваиваем  head следущий элемент
-                }
-                return resValue; //возвращаем удаляемый элемент
-            }
-            // нужно пробежаться до того элемента, который мы хотим удалить
-            Node curNode = head;
-            Node prevNode = head; // node до curNode
-            int count = 0; // создаём счетчик
-            while ((curNode = curNode.getNext()) != null) { // бежим до конца масссива
-                count++; //увеличиваем счётчик на 1
-                if (count == index) { //если счётчик равен искомому индексу, значит нашли нужный элемент
-                    break; //выходим из цикла
-                }
-                prevNode = prevNode.getNext(); //присваиваем предыдущему значению стоящий после него элемент
-            }
-            //если удаляем с конца
-            if (curNode == null) // проверяем если curNode не null
-                return null;
-            Object resValue = curNode.getValue(); //присваиваем переменной значение текущего элемента
-
-            if (curNode.getNext() == null) { //проверяем, если текущий элемент последний
-                prevNode.setNext(null); //зануляем элемент после предыдущего элемента
-            } else {
-                prevNode.setNext(curNode.getNext()); //присваиваем элементу после предыдущего, значение следующего за текущим
-                curNode.setNext(null); // зануляем следующий элемент после текущего
-            }
-
-            return resValue;  //возвращаем значение удаляемого элемента
-        }
+        return resValue;  //возвращаем значение удаляемого элемента
+    }
 
     private void checkIndex(int index) {
         if (!isCorrectIndex(index)) {
@@ -161,13 +160,13 @@ public class MyLinkedList {
         return false;
     }
 
-        public int indexOf (Object o){
-            return 0;
-        }
+    public int indexOf(Object o) {
+        return 0;
+    }
 
-        public int lastIndexOf (Object o){
-            return 0;
-        }
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
 
 
     @Override
